6ebc9e5fe29c37c187cd340dd1ae6760a47e4655
liltmagicbox <68154458+liltmagicbox@users.noreply.github.com>
2020-12-06 18:47:26 +0900

    add some files
    tamplates or else

--- timemaker.py
+++ timemaker.py
@@ -13,3 +13,10 @@ def datestr():
     now = datetime.datetime.now()
     now=now.astimezone(tzinfo)
     return now.strftime("%Y.%m.%d %H:%M:%S")
+
+#datetime.datetime.utcfromtimestamp(1606644954)
+#datetime.datetime(2020, 11, 29, 10, 15, 54)
+#datetime.datetime.utcfromtimestamp(1606644954+(9*3600))
+#datetime.datetime(2020, 11, 29, 19, 15, 54)
+def datestrstamp(timestamp):
+    return datetime.datetime.utcfromtimestamp(timestamp+(9*3600)).strftime("%Y.%m.%d %H:%M:%S")
